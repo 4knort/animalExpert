7842f33cff26d3cc9d5eaee6757174b1e58f143e
4knort <4knort@gmail.com>
2016-05-01 18:10:13 +0300

    css comb

--- source/js/script.js
+++ source/js/script.js
@@ -5,6 +5,7 @@
   var modalLogin = document.querySelector('.modal-login');
 
   inputContainer.addEventListener('click', function(event){
+    event.preventDefault();
     if (event.target.classList.contains('product-form__count-btn--plus')) {
       if (parseInt(input.value) < 50) {
         input.value = parseInt(input.value) + 1;
@@ -15,7 +16,7 @@
         input.value = parseInt(input.value) - 1;
       }
     }
-  });
+  })
 
   login.addEventListener('click', function(event) {
     event.preventDefault();
